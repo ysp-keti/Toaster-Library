7e840da256d36a699256ed5cb5b282cfc91a7c5b
Yong-Suk Park <yspark.keti@gmail.com>
2022-10-17 15:18:49 +0900

    Initial Commit

---
+++ ToasterLibrary/src/main/java/org/yspark/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,11 @@
+package org.yspark.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+}
